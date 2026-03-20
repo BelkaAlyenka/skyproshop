49fe13a199483cdb06dffdb3f4d3071c1ea320ea
BelkaAlyenka <alakrinskaaalena@gmail.com>
2026-03-20 12:53:56 +0700

    First commit in this project

---
+++ src/test/java/com/example/skyproshop/SkyproshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skyproshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
